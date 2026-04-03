102168a1b89dd66545ce19644c84baaf185c1800
Murilo Machado Hoffman de Oliveira <165489051+MuriloMHO@users.noreply.github.com>
2026-04-03 12:00:32 +0100

    Update blih.js

--- blih.js
+++ blih.js
@@ -1,9 +1,17 @@
-let  countBs = function(word){
-    let flag = 0
-    for (let num = 0; num < word.length; num + 1) {
-        if (word[num] == "B") {
-            flag += 1
+let range = function (start, end, step = start < end ? 1 : -1){
+    rangeList = []
+
+    if (step > 0) {
+        for (let rangeCount = start; rangeCount <= end; rangeCount += step){
+            rangeList.push(rangeCount)
+        }
+    } else {
+        for (let rangeCount = start; rangeCount >= end; rangeCount += step){
+            rangeList.push(rangeCount)
         }
     }
-    return flag
+
+    return rangeList;
 }
+
+console.log(range(7, 10))
